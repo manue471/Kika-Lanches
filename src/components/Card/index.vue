a26baface5d98ee00d140c4bc801c3c0066c1b03
Pablo Emanuel <maneu471@gmail.com>
2025-08-18 15:37:02 -0300

    Changes

---
+++ src/components/Card/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  icon?: string;
+}>();
+</script>
+
+<template>
+    <div class="card">
+        <div class="icon">
+        <img :src="icon" alt="Icon" />
+        </div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<style scoped>
+.card {
+  height: 300px;
+  background: white;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  width: 500px;
+  justify-content: center;
+  gap: 3rem;
+}
+
+h1 {
+  color: black;
+  font-size: 16px;
+}
+</style>
